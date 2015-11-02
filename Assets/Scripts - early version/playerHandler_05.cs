5d9cdaec065b034af54bc2cc709d3b44a9ef26b3
Thorn5n101 <Thornston101@gmail.com>
2015-11-02 04:22:32 +0000

    skip1

---
+++ Assets/Scripts - early version/playerHandler_05.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerHandler_05 : MonoBehaviour {
+	
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	
+	public void jump(){
+		
+		this.GetComponent<Rigidbody2D>().AddForce (Vector2.up * 3000);
+		
+	}
+	
+	
+}
